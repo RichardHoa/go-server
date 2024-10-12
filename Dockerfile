c7d1b0a6759beb783e0a09567f03e3a88348050d
RichardHoa <richardhoa.work@gmail.com>
2024-10-12 15:48:02 +0700

    nice

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM debian:stable-slim
-
-# COPY source destination
-COPY go-server /go-server
-
-CMD ["./go-server"]
-
--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -17,7 +17,6 @@ var (
 	mutex   sync.Mutex
 )
 
-// HandlerReadiness handles the /healthz endpoint
 func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
